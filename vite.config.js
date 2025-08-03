2472d9f2b6028641ddccd17a48b67775e6a92d02
Minglemates <ami.minglemates@gmail.com>
2025-08-03 11:54:39 +0200

    Mise à jour index.html et vite.config.js pour le déploiement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/visionneuse/',  // base pour GitHub Pages sous un sous-dossier
   plugins: [react()],
 })
